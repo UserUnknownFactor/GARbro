945d82690341297245ed769770c0cad755d00da3
UserUnknownFactor <63057995+UserUnknownFactor@users.noreply.github.com>
2025-07-23 14:07:04 +0300

    Removed license headers

--- ArcFormats/Unity/AudioFSB5.cs
+++ ArcFormats/Unity/AudioFSB5.cs
@@ -1,32 +1,3 @@
-//! \file       AudioFSB5.cs
-//! \date       Thu Apr 06 01:12:27 2017
-//! \brief      FMOD Sample Bank audio file.
-//
-// Based on [python-fsb5](https://github.com/HearthSim/python-fsb5)
-//
-// Copyright (c) 2016 Simon Pinfold
-//
-// C# implementation Copyright (C) 2017 by morkt
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-//
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
